809c1865cb394be49d4ce5c52175b518b3543195
SA-Ash <sai.ashrith2605@gmail.com>
2026-01-03 19:17:49 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ import userRouter from "./routes/user.route.js";
 const app = express()
 config()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 }));
